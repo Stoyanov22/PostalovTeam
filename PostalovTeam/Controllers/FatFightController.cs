0c543de4e9aa7db327423ae19e4e19b8a57bd9d9
Iliyan Stoyanov <virus_22@abv.bg>
2019-02-10 16:28:44 +0200

    ASP .NET Framework Website Created

---
+++ PostalovTeam/Controllers/FatFightController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostalovTeam.Controllers
+{
+    public class FatFightController : Controller
+    {
+        // GET: FatFight
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PostalovTeam/Controllers/KangooJumpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PostalovTeam.Controllers
+{
+    public class KangooJumpController : Controller
+    {
+        // GET: KangooJump
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
