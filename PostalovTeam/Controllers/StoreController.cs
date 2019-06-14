d22ce560d153957a447f8445ab4c3d168025f54c
Iliyan Stoyanov <virus_22@abv.bg>
2019-06-14 21:35:29 +0300

    Commit before reinstalling OS.  Shopping card, rejim and email left

--- PostalovTeam/Controllers/StoreController.cs
+++ PostalovTeam/Controllers/StoreController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PostalovTeam.Models;
+using File = PostalovTeam.Models.File;
 
 namespace PostalovTeam.Controllers
 {
@@ -63,23 +66,32 @@ namespace PostalovTeam.Controllers
         }
 
         [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult DeleteCategory(int categoryId)
+        public ActionResult DeleteCategory(int? categoryId)
         {
-            var category = pte.Categories.SingleOrDefault(x => x.CategoryId == categoryId);
+            Category category = pte.Categories.Find(categoryId);
+            return View(category);
+        }
+
+        // POST: Test/Delete/5
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult DeleteCategoryConfirmed(int id)
+        {
+            Category category = pte.Categories.Find(id);
             pte.Categories.Remove(category);
+            pte.SaveChanges();
             return RedirectToAction("Index", "Store");
         }
 
         public ActionResult Products(int categoryId)
         {
-            var products = pte.Products.Where(p => p.CategoryId == categoryId).ToList();
+            var products = pte.Products.Where(p => p.CategoryId == categoryId).Include("File").ToList();
             return View(products);
         }
 
         public ActionResult Product(int productId)
         {
-            var product = pte.Products.SingleOrDefault(x => x.ProductId == productId);
+            var product = pte.Products.Include("File").SingleOrDefault(x => x.ProductId == productId);
             return View(product);
         }
 
@@ -100,13 +112,26 @@ namespace PostalovTeam.Controllers
                 {
                     Name = newProduct.Name,
                     Price = newProduct.Price,
-                    CategoryId = newProduct.CategoryId
+                    CategoryId = newProduct.CategoryId,
+
                 };
                 pte.Products.Add(product);
+
+                byte[] fileData = null;
+                using (var binaryReader = new BinaryReader(Request.Files["photo"].InputStream))
+                {
+                    fileData = binaryReader.ReadBytes(Request.Files["photo"].ContentLength);
+                }
+                var photo = new PostalovTeam.Models.File
+                {
+                    Content = fileData,
+                };
+                File file = pte.Files.Add(photo);
+                product.File = file;
                 pte.SaveChanges();
             }
-
             return RedirectToAction("Index", "Store");
+
         }
 
         [Authorize(Roles = "Admin")]
@@ -132,11 +157,20 @@ namespace PostalovTeam.Controllers
         }
 
         [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult DeleteProduct(int productId)
+        public ActionResult DeleteProduct(int? productId)
         {
-            var product = pte.Products.SingleOrDefault(x => x.ProductId == productId);
+            Product product = pte.Products.Find(productId);
+            return View(product);
+        }
+
+        // POST: Test/Delete/5
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult DeleteProductConfirmed(int id)
+        {
+            Product product = pte.Products.Find(id);
             pte.Products.Remove(product);
+            pte.SaveChanges();
             return RedirectToAction("Index", "Store");
         }
 
